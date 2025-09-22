dc7eb7cd43568d99b21b77cc54c10f7a82e181c8
sakil shaikh <sakilxotwod@gmail.com>
2025-09-22 22:49:13 +0530

    code of item.h

---
+++ include/item.h
@@ -0,0 +1,17 @@
+
+#ifndef ITEM_H
+#define ITWM_H
+
+#include <string>
+
+class Item{
+    public:
+        std::string name;
+        std::string type;
+        int value;
+
+        Item(std::string n, std::string t, int v);
+       
+};
+
+#endif
